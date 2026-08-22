9bfe24327784f5d67994a4394529c3f47c47bd8a
allanmeng <allanmeng@users.noreply.github.com>
2026-08-23 04:31:23 +0800

    ui: swap title-side logos with their styles

--- scripts/generate_pages.py
+++ scripts/generate_pages.py
@@ -428,7 +428,7 @@ INDEX_HTML = """<!DOCTYPE html>
 <div class="container">
 
   <header>
-    <h1><img class="logo" src="assets/Intel_Graphics_logo.png" alt="Intel Graphics">IntelGpu-ComfyUI-Collection<img class="logo-group" src="assets/group_logo_80.png" alt="Group"></h1>
+    <h1><img class="logo-group" src="assets/group_logo_80.png" alt="Group">IntelGpu-ComfyUI-Collection<img class="logo" src="assets/Intel_Graphics_logo.png" alt="Intel Graphics"></h1>
     <p class="note">以下项目多数来自社群，少量官方提供</p>
     <div class="chips">
 __NAV__
@@ -556,7 +556,7 @@ LINKS_HTML = """<!DOCTYPE html>
 <div class="container">
 
   <header>
-    <h1><img class="logo" src="assets/Intel_Graphics_logo.png" alt="Intel Graphics">IntelGpu-ComfyUI-Collection<img class="logo-group" src="assets/group_logo_80.png" alt="Group"></h1>
+    <h1><img class="logo-group" src="assets/group_logo_80.png" alt="Group">IntelGpu-ComfyUI-Collection<img class="logo" src="assets/Intel_Graphics_logo.png" alt="Intel Graphics"></h1>
     <p class="note">Intel XPU 重要组件下载地址</p>
     <div class="chips">
 __NAV__
